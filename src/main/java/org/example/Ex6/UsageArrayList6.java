790598e65e62b2cebf3f978bbec81638a1970236
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-14 19:32:22 +0100

    Update UsageArrayList6.java

--- src/main/java/org/example/Ex6/UsageArrayList6.java
+++ src/main/java/org/example/Ex6/UsageArrayList6.java
@@ -1,1 +1,10 @@
-package org.example.Ex6;/* Перебрать ArrayList и найти наибольшую возрастающую последовательность элементов.Перебрать ArrayList и удалить все дубликаты элементов.Перебрать ArrayList и создать новый список, содержащий только уникальные строки.Перебрать ArrayList и объединить все строки в одну с использованием разделителя. */public class UsageArrayList6 {    public static void main(String[] args) {    }}
+package org.example.Ex6;
+/* Перебрать ArrayList и найти наибольшую возрастающую последовательность элементов.
+Перебрать ArrayList и удалить все дубликаты элементов.
+Перебрать ArrayList и создать новый список, содержащий только уникальные строки.
+Перебрать ArrayList и объединить все строки в одну с использованием разделителя. */
+public class UsageArrayList6 {
+    public static void main(String[] args) {
+
+    }
+}
