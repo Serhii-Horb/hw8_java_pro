63cebd6640b8169997b72394261dae439f220a6d
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-14 19:31:08 +0100

    Update UsageArrayList2.java

--- src/main/java/org/example/Ex2/UsageArrayList2.java
+++ src/main/java/org/example/Ex2/UsageArrayList2.java
@@ -1,1 +1,43 @@
-package org.example.Ex2;import java.util.ArrayList;import java.util.List;public class UsageArrayList2 {    public static void main(String[] args) {        // Перебрать ArrayList и найти наименьший элемент.        List<Integer> list = List.of(1, 2, 3, 4, 5);        int minElement = list.get(0);        for (Integer integer : list) {            if (integer < minElement) {                minElement = integer;            }        }        System.out.println(minElement);        System.out.println("---------------------------------");        // Перебрать ArrayList и найти количество элементов, начинающихся с определенной буквы.        List<String> stringList = List.of("Tom", "Jon", "Sam", "Karl", "Pol", "Tim", "Piter", "Petra");        char sample = 'P';        int countChar = 0;        for (String s : stringList) {            if (s.charAt(0) == sample) {                countChar++;            }        }        System.out.println(countChar);        System.out.println("---------------------------------");        // Создать ArrayList с объектами вашего собственного класса и удалить все элементы, удовлетворяющие определенному условию        List<Kinder> kinderList = new ArrayList<>(List.of(new Kinder("Катя", 18), new Kinder("Оля", 20),                new Kinder("Тамара", 5), new Kinder("Валера", 7), new Kinder("Марк", 2)));        final int removeAge = 18;        for (int i = 0; i < kinderList.size(); i++) {            if (kinderList.get(i).getAge() > removeAge) {                kinderList.remove(i);            }        }        System.out.println(kinderList);    }}
+package org.example.Ex2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsageArrayList2 {
+    public static void main(String[] args) {
+
+        // Перебрать ArrayList и найти наименьший элемент.
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
+        int minElement = list.get(0);
+        for (Integer integer : list) {
+            if (integer < minElement) {
+                minElement = integer;
+            }
+        }
+        System.out.println(minElement);
+        System.out.println("---------------------------------");
+
+        // Перебрать ArrayList и найти количество элементов, начинающихся с определенной буквы.
+        List<String> stringList = List.of("Tom", "Jon", "Sam", "Karl", "Pol", "Tim", "Piter", "Petra");
+        char sample = 'P';
+        int countChar = 0;
+        for (String s : stringList) {
+            if (s.charAt(0) == sample) {
+                countChar++;
+            }
+        }
+        System.out.println(countChar);
+        System.out.println("---------------------------------");
+
+        // Создать ArrayList с объектами вашего собственного класса и удалить все элементы, удовлетворяющие определенному условию
+        List<Kinder> kinderList = new ArrayList<>(List.of(new Kinder("Катя", 18), new Kinder("Оля", 20),
+                new Kinder("Тамара", 5), new Kinder("Валера", 7), new Kinder("Марк", 2)));
+        final int removeAge = 18;
+        for (int i = 0; i < kinderList.size(); i++) {
+            if (kinderList.get(i).getAge() > removeAge) {
+                kinderList.remove(i);
+            }
+        }
+        System.out.println(kinderList);
+    }
+}
