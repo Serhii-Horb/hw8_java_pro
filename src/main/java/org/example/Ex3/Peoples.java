fb2680d6b3bbd00ce0ca3a258a5f7ebce50f999c
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-14 19:31:23 +0100

    Update Peoples.java

--- src/main/java/org/example/Ex3/Peoples.java
+++ src/main/java/org/example/Ex3/Peoples.java
@@ -1,1 +1,35 @@
-package org.example.Ex3;public class Peoples {private String name;private int age;    public Peoples(String name, int age) {        this.name = name;        this.age = age;    }    public String getName() {        return name;    }    public void setName(String name) {        this.name = name;    }    public int getAge() {        return age;    }    public void setAge(int age) {        this.age = age;    }    @Override    public String toString() {        return "Peoples{" +                "name='" + name + '\'' +                ", age=" + age +                '}';    }}
+package org.example.Ex3;
+
+public class Peoples {
+private String name;
+private int age;
+
+    public Peoples(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Peoples{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
